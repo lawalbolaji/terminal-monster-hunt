eca05e3a0c6087a50fb6016278c24f6adb5ebd16
Abdulrasheed Lawal <lawalabdulrasheedbolaji@gmail.com>
2021-12-09 17:19:02 +0100

    fix validation errors for play_again()

--- utils/playAgain.py
+++ utils/playAgain.py
@@ -1,5 +1,7 @@
 def play_again():
     result = input("Would you like to play again? (y/n)")
+    while(result != 'y' and result != 'n'):
+        result = input("please enter a valid response")
     if(result == 'y'):
         return True
     return False
