73deb11ef78c332f0e6bb66baaee692e1d8ebb13
Abdulrasheed Lawal <lawalabdulrasheedbolaji@gmail.com>
2021-12-09 17:13:49 +0100

    fix linting errors

--- instructions.py
+++ instructions.py
@@ -5,11 +5,14 @@ current_beast = beast()
 current_weapon = weapon()
 
 start = [
-    "You find yourself standing in an open field, filled with grass and yellow wildflowers.",
-    "Rumor has it that a troll is somewhere around here, and has been terrifying the nearby village.",
+    ("You find yourself standing in an open field,"
+        "filled with grass and yellow wildflowers."),
+    ("Rumor has it that a troll is somewhere around here,"
+        "and has been terrifying the nearby village."),
     "In front of you is a house.",
     "To your right is a dark cave.",
-    "In your hand you hold your trusty (but not very effective) dagger.\n"]
+    ("In your hand you hold your trusty "
+        "(but not very effective) dagger.\n")]
 
 decide = [
     "Enter 1 to knock on the door of the house.",
@@ -21,7 +24,8 @@ decide = [
 
 house = [
     "You approach the door of the house.",
-    f"You are about to knock when the door opens and out steps a {current_beast}.",
+    ("You are about to knock when the door "
+        f"opens and out steps a {current_beast}."),
     f"Eep! This is the {current_beast}'s house!",
     f"The {current_beast} attacks you!",
     "Would you like to (1) fight or (2) run away?"  # prompt
@@ -32,7 +36,8 @@ cave = [
     "It turns out to be only a very small cave.",
     "Your eye catches a glint of metal behind a rock.",
     f"You have found the {current_weapon}",
-    f"You discard your silly old dagger and take the {current_weapon} with you.",
+    (f"You discard your silly old dagger and "
+        "take the {current_weapon} with you."),
     "You walk back out to the field.\n",
 ]
 
@@ -43,12 +48,16 @@ fight_no_sword = [
 ]
 
 fight_with_sword = [
-    f"As the {current_beast} moves to attack, you bring our your {current_weapon}",
-    f"The {current_weapon} shines brightly in your hand as you brace yourself for the attack.",
-    f"But the {current_beast} takes one look at your shiny new toy and runs away!",
+    (f"As the {current_beast} moves to attack, "
+        f"you bring our your {current_weapon}"),
+    (f"The {current_weapon} shines brightly"
+        "in your hand as you brace yourself for the attack."),
+    (f"But the {current_beast} takes one look at your "
+        "shiny new toy and runs away!"),
     f"You have rid the town of the {current_beast}. You are victorious!",
 ]
 
 escape = [
-    "You run back into the field. Luckily, you don't seem to have been followed."
+    ("You run back into the field. Luckily, "
+        "you don't seem to have been followed.")
 ]
